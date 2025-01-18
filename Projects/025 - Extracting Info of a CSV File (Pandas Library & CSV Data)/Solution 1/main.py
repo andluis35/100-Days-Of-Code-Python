1b9712d59bf01678882dfec2a6ca6a87a24603f2
Anderson Luis <and.luis35@gmail.com>
2025-01-18 17:53:35 -0300

    Projeto 025 - Solução 1 finalizada

---
+++ Projects/025 - Extracting Info of a CSV File (Pandas Library & CSV Data)/Solution 1/main.py
@@ -0,0 +1,26 @@
+import pandas
+
+original_data = pandas.read_csv("./2018_Central_Park_Squirrel_Census_-_Squirrel_Data.csv")
+gray_squirrels_count = 0
+cinnamon_squirrels_count = 0
+black_squirrels_count = 0
+
+for color in original_data["Primary Fur Color"]:
+    if color == "Gray":
+        gray_squirrels_count += 1
+    if color == "Cinnamon":
+        cinnamon_squirrels_count += 1
+    if color == "Black":
+        black_squirrels_count += 1
+
+data_dict = {
+    "Fur Color": ["Gray", "Cinnamon", "Black"],
+    "Count": [gray_squirrels_count, cinnamon_squirrels_count, black_squirrels_count]
+}
+
+new_data = pandas.DataFrame(data_dict)
+
+print(new_data)
+gray_squirrels_count = original_data[original_data["Primary Fur Color"] == "Gray"]
+cinnamon_squirrels_count = original_data[original_data["Primary Fur Color"] == "Cinnamon"]
+black_squirrels_count = original_data[original_data["Primary Fur Color"] == "Black"]
